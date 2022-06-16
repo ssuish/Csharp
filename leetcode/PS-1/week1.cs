228792047752c619cf7abdfd61161d109e06a7fe
suisoh <88411618+ssuish@users.noreply.github.com>
2022-06-16 11:16:16 +0800

    new final

--- leetcode/PS-1/week1.cs
+++ leetcode/PS-1/week1.cs
@@ -66,10 +66,36 @@ public class Week1
         Given an integer number n, return the difference between 
         the product of its digits and the sum of its digits.
     */
+    public List<int> numl = new List<int>();
+
+    public void ToList(int n)
+    {
+        string temp = n.ToString();
+        for (int i = 0; i < temp.Length; i++)
+        {
+            numl.Add(temp[i] - '0');
+        }
+    }
+
+    public int SumNum(int n)
+    {
+        int sum = 0;
+        for (int i = 0; i < numl.Count; i++)
+            sum += numl[i];
+        return sum;
+    }
+
+    public int ProductNum(int n)
+    {
+        int prod = 1;
+        for (int i = 0; i < numl.Count; i++)
+            prod *= numl[i];
+        return prod;
+    }
+
     public int SubtractProductAndSum(int n)
     {
-        
-        
-        return 0;
+        ToList(n);
+        return ProductNum(n) - SumNum(n);
     }
 }
--- leetcode/Program.cs
+++ leetcode/Program.cs
@@ -1,6 +1,5 @@
 ﻿using leetcode;
 
 Week1 wk = new();
-int[] arr = {8000, 9000, 2000, 3000, 6000, 1000};
-double a = wk.Average(arr);
-Console.WriteLine(a);
+int n = 4421;
+Console.WriteLine(wk.SubtractProductAndSum(n));
