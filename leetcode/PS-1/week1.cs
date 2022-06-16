25948a841bde739fe528067787931ce63b571adb
suisoh <88411618+ssuish@users.noreply.github.com>
2022-06-16 10:18:43 +0800

    nwq

--- leetcode/PS-1/week1.cs
+++ leetcode/PS-1/week1.cs
@@ -57,8 +57,19 @@ public class Week1
     */
     public int HammingWeight(uint n)
     {
-        
 
+
+        return 0;
+    }
+
+    /*
+        Given an integer number n, return the difference between 
+        the product of its digits and the sum of its digits.
+    */
+    public int SubtractProductAndSum(int n)
+    {
+        
+        
         return 0;
     }
 }
