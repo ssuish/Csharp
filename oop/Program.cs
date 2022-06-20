8c902e040b041533909e28e63100305af460586d
suisoh <88411618+ssuish@users.noreply.github.com>
2022-06-20 14:47:15 +0800

    links

--- oop/Program.cs
+++ oop/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// ===========================================================
+// OOP Basics: Classes and Objects
+/*
+    Links for reference: 
+    - https://zetcode.com/lang/csharp/oopi/
+    - https://zetcode.com/lang/csharp/oopii/
+    - https://zetcode.com/lang/csharp/methods/
+    - https://zetcode.com/lang/csharp/properties/
+    - https://zetcode.com/lang/csharp/structures/
+    - https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/classes-and-structs
+*/
+
