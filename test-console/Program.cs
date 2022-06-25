244a603113d95db014fa504b2c91a6832ea0b7c8
suisoh <88411618+ssuish@users.noreply.github.com>
2022-06-25 20:07:55 +0800

    new program

--- test-console/Program.cs
+++ test-console/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 // Declare and test codes here.
+using MultiMenu;
+
+var app = new App();
+app.Run();
--- test-console/src/Multi-Menu.cs
+++ test-console/src/Multi-Menu.cs
@@ -1,2 +1,33 @@
 // Multi-Menus in Console App
+namespace MultiMenu;
 
+public class App
+{
+    public int Run()
+    {
+        // Only codes here are executed.
+
+        bool cky = true;
+        while (cky)
+        {
+            Console.WriteLine("Welcome");
+            Console.WriteLine("GOTO: [0] Menu A [1] Menu B [Esc] Exit");
+            if (Console.ReadKey(true).Key == ConsoleKey.D0)
+            {
+                
+            }
+            else if (Console.ReadKey(true).Key == ConsoleKey.D1)
+            {
+
+            }
+            else if (Console.ReadKey(true).Key == ConsoleKey.Escape)
+            {
+                cky = false;
+            }
+        }
+
+        return 0;
+    }
+
+
+}
