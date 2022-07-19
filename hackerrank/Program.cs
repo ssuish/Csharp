c0cfa29dc76c025b20108654cc40b894d0c95fab
suisoh <88411618+ssuish@users.noreply.github.com>
2022-07-19 23:41:00 +0800

    new file

--- hackerrank/Program.cs
+++ hackerrank/Program.cs
@@ -1,4 +1,12 @@
 ﻿using Hackerrank.Algorithms;
 
-string date = "07:05:45PM";
-Console.WriteLine(TimeConverter.timeConversion(date));
+var grades = new List<int>(){
+    73, 67, 38, 33, 58, 37
+};
+
+var finalgrade = GradeScores.gradingStudents(grades);
+
+foreach(int grade in finalgrade)
+{
+    Console.WriteLine(grade);
+}
---
+++ hackerrank/src/algorithms/implementation/wk3.cs
@@ -0,0 +1,20 @@
+// Implementation of Algorithms
+
+/*
+    Grading System
+    Parameters: int[n] of grades before rounding off.
+    Returns: int[n] of grades after rounding off.
+    Constraints: 1 <= n <= 60; 0 < int[n] < 100.
+
+    Instruction:
+    ~ student grades recieves from 0 - 100.
+    ~ any grade < 40 is a failing grade.
+    ~ if (grades - nearest multiple of 5) < 3, round the
+    grade up to next multiple of 5.
+    ~ if grades < 38, no rounding off.
+*/
+
+class GradeScores
+{
+   
+}
