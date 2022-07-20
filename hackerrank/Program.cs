94a51b52333ebeaf48ce5a831bef777465dc6c5c
suisoh <88411618+ssuish@users.noreply.github.com>
2022-07-20 19:28:53 +0800

    unsolved problem 1

--- hackerrank/Program.cs
+++ hackerrank/Program.cs
@@ -1,12 +1,2 @@
 ﻿using Hackerrank.Algorithms;
 
-var grades = new List<int>(){
-    73, 67, 38, 33, 58, 37
-};
-
-var finalgrade = GradeScores.gradingStudents(grades);
-
-foreach(int grade in finalgrade)
-{
-    Console.WriteLine(grade);
-}
--- hackerrank/src/algorithms/implementation/wk3.cs
+++ hackerrank/src/algorithms/implementation/wk3.cs
@@ -12,9 +12,79 @@
     ~ if (grades - nearest multiple of 5) < 3, round the
     grade up to next multiple of 5.
     ~ if grades < 38, no rounding off.
-*/
+
 
 class GradeScores
 {
-   
+    private static List<int> studentGrades = new List<int>();
+
+    private static int GetNearestMultiple(int grade)
+    {
+        // Find nearest multiple of 5 of grade. 
+        const int multiple = 5;
+        int nearestMultiple = grade;
+
+        if (grade < multiple)
+            return grade;
+
+        nearestMultiple = grade + multiple/2;
+        nearestMultiple -= (nearestMultiple % multiple);
+        return nearestMultiple;
+    }
+
+    public static List<int> gradingStudents(List<int> grades)
+    {
+        int multiple = 0;
+
+        foreach (int grade in grades)
+        {
+            multiple = GetNearestMultiple(grade);
+            int difference = 0;
+            
+            if (grade < 38)
+            {
+                studentGrades.Add(grade);
+            }
+            else if (grade > multiple) 
+            {
+                multiple += 5;
+                difference = multiple - grade;
+                if (difference < 3)
+                {
+                    studentGrades.Add(multiple);
+                }
+                else
+                {
+                    studentGrades.Add(grade);
+                }
+            }
+            else if (grade < multiple)
+            {
+                difference = multiple - grade;
+                if (difference < 3)
+                {
+                    studentGrades.Add(multiple);
+                }
+                else
+                {
+                    studentGrades.Add(grade);
+                }
+            }
+        }
+
+        return studentGrades;
+    }
 }
+
+============= Unsolved! ================
+*/
+
+/*
+    Function Squares
+    Parameters: int a = lower bound, int b = upper bound.
+    Returns: int: number of square integers in the range.
+
+    Instruction:
+    It should return an integer representing the number of 
+    square integers in the inclusive range from a to b.
+*/
